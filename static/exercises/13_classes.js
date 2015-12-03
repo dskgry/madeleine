3448acf6e880f9189843208cee17fa1f0d06e6fa
git <sven.koelpin@outlook.com>
2015-12-03 20:39:37 +0100

    fixed a test that i destroyed. I'm sorry renke10

--- static/exercises/13_classes.js
+++ static/exercises/13_classes.js
@@ -1,18 +1,18 @@
 describe("", function () {
-    it("Create a Person-class. Its constructor takes 'firstName' and 'lastName' as parameters and save these parameters as properties.", function() {
+    it("Create a Person class. Its constructor takes 'firstName' and 'lastName' as parameters and save these parameters as properties.", function() {
       var lars = new Person("Lars", "Kölpin");
 
       expect(lars.firstName).toBe("Lars");
       expect(lars.lastName).toBe("Kölpin");
     });
 
-    it("Add a method 'toString' to the Person-class. The method should return the persons' full name (space separated)", function() {
+    it("Add a method 'toString' to the Person class. The method should return the persons' full name (space separated)", function() {
       var lars = new Person("Lars", "Kölpin");
 
       expect(lars.toString()).toBe("Lars Kölpin");
     });
 
-    it("Create a Student-class that extends the Person-class. The Students' constructor should accept the same params the Person does.", function() {
+    it("Create a Student class that extends the Person class. The Students' constructor should accept the same params the Person does.", function() {
       var studentLars = new Student("Lars", "Kölpin");
 
       expect(studentLars.firstName).toBe("Lars");
@@ -28,7 +28,7 @@ describe("", function () {
     it("Add a constructor that accepts an additional third parameter called 'age'. Add a method 'getAge' that returns the students' age.", function() {
       var studentLars = new Student("Lars", "Kölpin", 21);
 
-      expect(studentLars.toString()).toBe("Student Lars Kölpin");
+      expect(studentLars.toString()).toBe("Student_Lars Kölpin");
       expect(studentLars.getAge()).toBe(21);
     })
 });
